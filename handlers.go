e0bfd31eb04d488a56840ddb1ed0a0d7a4e106b5
Ross Kippenbrock <rkippenbrock@gmail.com>
2016-01-14 22:41:17 -0500

    add a create todo function

--- handlers.go
+++ handlers.go
@@ -4,7 +4,8 @@ import (
     "encoding/json"
     "fmt"
     "net/http"
-
+    "io"
+    "io/ioutil"
     "github.com/gorilla/mux"
 )
 
@@ -13,12 +14,7 @@ func Index(w http.ResponseWriter, r *http.Request) {
 }
 
 func TodoIndex(w http.ResponseWriter, r *http.Request) {
-    todos := Todos{
-        Todo{Name: "Write presentation"},
-        Todo{Name: "Host meetup"},
-    }
-
-    w.Header().Set("Content-Type", "application/json; charset=UFT-8")
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
     w.WriteHeader(http.StatusOK)
     if err := json.NewEncoder(w).Encode(todos); err != nil {
         panic(err)
@@ -29,4 +25,29 @@ func TodoShow(w http.ResponseWriter, r *http.Request) {
     vars := mux.Vars(r)
     todoId := vars["todoId"]
     fmt.Fprintln(w, "Todo show:", todoId)
+}
+
+func TodoCreate(w http.ResponseWriter, r *http.Request) {
+    var todo Todo
+    body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+    if err != nil {
+        panic(err)
+    }
+    if err := r.Body.Close(); err != nil {
+        panic(err)
+    }
+    if err := json.Unmarshal(body, &todo); err != nil {
+        w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+        w.WriteHeader(422) // unprocessable entity
+        if err := json.NewEncoder(w).Encode(err); err != nil {
+            panic(err)
+        }
+    }
+
+    t := RepoCreateTodo(todo)
+    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+    w.WriteHeader(http.StatusCreated)
+    if err := json.NewEncoder(w).Encode(t); err != nil {
+        panic(err)
+    }
 }
